e6bd4c2413daeed6a72f32537e97605eae60287d
BeaPc <developer.beacandido@gmail.com>
2022-12-29 22:42:36 -0300

    feat: update

--- hello-word/salario_real.py
+++ hello-word/salario_real.py
@@ -1,3 +1,11 @@
 wage = int(input('wage? '))
 tax = float (input ('Tax in % (ex:27.5)?'))
 print ("Real value:{0}". format (wage-(tax * 0.01))) 
+if tax >= 10. and tax <= 27.:  
+   print("baixo")
+elif tax > 27. and tax <= 27.:
+   print ("Medio")
+elif tax > 27. and tax < 100.:
+   print("Alto")
+else :
+   print("Imposto invalido")         
