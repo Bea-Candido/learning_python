43e1600f0fd5732381d7234bb76ad9caa7a38c48
BeaPc <developer.beacandido@gmail.com>
2023-01-03 14:54:32 -0300

    feat: new module lista

---
+++ listas_dicionarios.py/lista.py
@@ -0,0 +1,19 @@
+minha_lista = ["abacaxi","melancia","abacate"]
+minha_lista_2=[1,2,3,4]
+minha_lista_3=["abaxaxi",2,9.85,True]
+
+print(minha_lista)
+print(minha_lista[2])
+
+for item in minha_lista:
+    print(item)
+
+tamanho = len(minha_lista)
+print(tamanho)
+
+minha_lista.append("limao")
+print(minha_lista)
+
+if 3 in minha_lista_2:
+   print("3 esta na lista")
+
---
+++ listas_dicionarios.py/lista_2.py
@@ -0,0 +1,3 @@
+lista = [ 30,27,9,12,24,18,15,24,6,3,0]
+lista.sort(reverse=True) 
+print(lista)
