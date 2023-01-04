c5d637ebc63aeeacc8dfa2966d6e13c3f06d35a5
BeaPc <developer.beacandido@gmail.com>
2023-01-04 15:07:29 -0300

    feat: examples

---
+++ exercicios/entrada_de_dados.py
@@ -0,0 +1,20 @@
+salario1: float; salario2: float 
+nome1: str; nome2: str 
+idade: int
+sexo: str 
+
+nome1= input("Nome da primeira pessoa:")
+salario1= float(input("Salario da primeira pessoa:"))
+
+nome2= input("Nome da segunda pessoa:")
+salario2= float(input("Salário da segunda pessoa:"))
+
+idade= int(input("DIgite uma idade:"))
+sexo= input("Digite um sexo(F/M):")
+
+print(f"Nome1: {nome1}")
+print(f"Salario1: {salario1}")
+print(f"Nome2: {nome2}")
+print(f"Salario2: {salario2}")
+print(f"Idade: {idade}")
+print(f"Sexo: {sexo}")
