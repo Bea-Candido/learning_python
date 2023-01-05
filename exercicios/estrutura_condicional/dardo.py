e8f4b55604d1ed1d98ec99c089d8fbcca5740e3b
BeaPc <developer.beacandido@gmail.com>
2023-01-05 20:24:49 -0300

    feat: new exercise

---
+++ exercicios/estrutura_condicional/dardo.py
@@ -0,0 +1,12 @@
+a= float; b= float; c= float;
+
+a= float(input("Digite a distancia A:"))
+b= float(input("Digite a distancia B:"))
+c= float(input("Digite a distancia C:"))
+
+if a > b and a > c :
+   print("Maior Distância: A")
+elif b > a and b > c :
+    print("Maior distancia: B")
+else:
+    print("Maior Distância C")       
