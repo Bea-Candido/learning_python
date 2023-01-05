0b263a1aa2319af729a88619808a4b29366ba8c1
BeaPc <developer.beacandido@gmail.com>
2023-01-05 20:53:25 -0300

    feat: new exercise

---
+++ exercicios/estrutura_condicional/temperatura.py
@@ -0,0 +1,15 @@
+celsius= float; fahrenheit= float; temperatura= float; escala= str;
+
+escala = str(input("Você vai digitar a temperatura em qual escala?(C/F)"))
+
+celsius = 5.0 / 9.0 (fahrenheit- 32.0) 
+
+fahrenheit = celsius * 9.0 / 5.0+ 32.0
+
+if escala == "f":
+     fahrenheit = float(input("Digite a temperatura em Fahrenheit"))  
+     print(f"Temperatura equivalente em Celsius:{celsius}")
+else: 
+    celsius= float(input("Digite a temperatura em Celsius: "))
+    print(f"Temperatura equivalente em Fahrenheit:{fahrenheit}")    
+     
