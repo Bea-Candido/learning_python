82738a425cd881b38dd2c90661b14f119587490f
BeaPc <developer.beacandido@gmail.com>
2023-01-04 21:57:32 -0300

    feat: new exercise

---
+++ exercicios/estrutura_sequencial/medidas.py
@@ -0,0 +1,14 @@
+a = float; b = float; c = float;
+quadrado= float; triangulo= float; trapezio= float;
+
+a= float(input("Digite a medida de A:"))
+b= float(input("Digite a medida de B:"))
+c= float(input("Digite a medida de C:"))
+
+quadrado = a * a 
+triangulo = (a*b) / 2
+trapezio = ((a+b)* c) / 2
+
+print(f"AREA DO QUADRADO:{quadrado}")
+print(f"AREA DO TRIANGULO:{triangulo}")
+print(f"AREA DO TRAPEZIO:{trapezio}")
