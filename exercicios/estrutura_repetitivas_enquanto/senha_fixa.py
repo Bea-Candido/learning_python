fac072ba73607ef11a745116836ae2b5885a9e30
BeaPc <developer.beacandido@gmail.com>
2023-01-06 10:29:49 -0300

    feat: new exercise

---
+++ exercicios/estrutura_repetitivas_enquanto/senha_fixa.py
@@ -0,0 +1,10 @@
+senha = int;
+
+senha = int(input("Digite a senha:"))
+
+while senha != 2022:
+      print("Senha invalida!Tente novamente:")
+      senha= int(input("Senha Invalida! Tente novamente:"))
+
+print("Acesso permitido")
+
