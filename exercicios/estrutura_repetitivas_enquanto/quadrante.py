9095be4de2671e8d8a378ae8c4ffa2fbd85dff01
BeaPc <developer.beacandido@gmail.com>
2023-01-06 10:41:03 -0300

    feat: new exercise

---
+++ exercicios/estrutura_repetitivas_enquanto/quadrante.py
@@ -0,0 +1,20 @@
+x = int; y = int;
+
+print("Digite os valores das coordenadas x e y :")
+x = int(input())
+y = int(input())
+
+while x != 0 and y != 0:
+    if x > 0 and y > 0:
+           print("Quadrante Q1")
+    elif x < 0 and y > 0:
+           print("Quadrante Q2")
+    elif x < 0 and y < 0: 
+           print("Quadrante Q3")
+    else:
+           print("Quadrante Q4")
+
+print("Digite os valores das coordenadas X e Y:")
+
+x = int(input())
+y = int(input())
