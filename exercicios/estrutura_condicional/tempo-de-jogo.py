2bbaffa7191a0f63692f0fde2b2aed92dc606f8c
BeaPc <developer.beacandido@gmail.com>
2023-01-05 21:25:49 -0300

    feat: new exercise

---
+++ exercicios/estrutura_condicional/tempo-de-jogo.py
@@ -0,0 +1,11 @@
+hInicial= int; hFinal= int; resp= int;
+
+hInicial= int(input("Hora inicial:"))
+hFinal= int(input("Hora final:"))
+
+if hInicial < hFinal:
+        resp = hFinal - hInicial
+else:
+       resp = 24 - (hInicial - hFinal)
+
+print(f"O jogo durou{resp}Horas(s)" )       
